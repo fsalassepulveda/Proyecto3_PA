b0f1cbe33a3b659d0c80a05866260d86669693c4
fsalassepulveda <38567467+fsalassepulveda@users.noreply.github.com>
2018-08-08 14:12:48 -0400

    Genera gráficos

--- ejercicio11.sh
+++ ejercicio11.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 cd $HOME
-cd 	Escritorio
+cd Escritorio
 mkdir Resultados
-cd  Resultados 
+cd Resultados 
 
 echo -e "#!/bin/bash \n
-wget -r -A .csv -nd  https://srvbioinf1.utalca.cl/~fduran/data" > Lista.txt
+wget -r -A .csv -nd  https://srvbioinf1.utalca.cl/~fduran/data
+wget -r -A .zip -nd https://srvbioinf1.utalca.cl/~fduran/scripts.zip
+unzip scripts.zip
+rm -r scripts.zip
+chmod 777 scripts
+cd scripts
+cp -a generateGraphic.py /scripts $HOME/Escritorio/Resultados
+" > Lista.txt
+
+
 
 bash Lista.txt
 
@@ -21,10 +30,10 @@ for i in `seq 0  5`; do
 	 
 done	
 
-cat Panguilemodetalle2010-2011.csv   |grep 2010 | sed 's/;/ /g' | sed 's/-/ /g' |awk '{print $2  " " $8}'>humedad_2010.txt
+cat Panguilemodetalle2010-2011.csv   |grep 2010 | sed 's/;/ /g' | sed 's/-/ /g' |awk '{print $2  " " $8}'> humedad_2010.txt
 cat Panguilemodetalle2010-2011.csv Panguilemodetalle2011-2012.csv  |grep 2011 | sed 's/;/ /g' | sed 's/-/ /g' |awk '{print $2  " " $8}'>humedad_2011.txt
 cat Panguilemodetalle2011-2012.csv Panguilemodetalle2012-2013.csv  |grep 2012 | sed 's/;/ /g' | sed 's/-/ /g' |awk '{print  $2  " " $8}'>humedad_2012.txt
-cat Panguilemodetalle2012-2013.csv Panguilemodetalle2013-2014.csv  |grep 2013 | sed 's/;/ /g' | sed 's/-/ /g' |awk '{print $2 " " $8}'>humedad_2013.txt
+cat Panguilemodetalle2012-2013.csv Panguilemodetalle2013-2014.csv  |grep 2013 | sed 's/;/ /g' | sed 's/-/ /g' |awk '{print $2 " " $8}' >humedad_2013.txt
 cat Panguilemodetalle2014-2015a.csv Panguilemodetalle2014-2015b.csv  |grep 2014 | sed 's/;/ /g' | sed 's/-/ /g' |awk '{print $2 " " $8}'>humedad_2014.txt
 cat Panguilemodetalle2014-2015a.csv Panguilemodetalle2014-2015b.csv  |grep 2015 | sed 's/;/ /g' | sed 's/-/ /g' |awk '{print $2 " " $8}'>humedad_2015.txt
 
@@ -33,21 +42,21 @@ cat Panguilemodetalle2010-2011.csv Panguilemodetalle2011-2012.csv  |grep 2011 | 
 cat Panguilemodetalle2011-2012.csv Panguilemodetalle2012-2013.csv  |grep 2012 | sed 's/;/ /g' | sed 's/-/ /g' |awk '{print  $2  " " $9}'>temperatura_2012.txt
 cat Panguilemodetalle2014-2015a.csv Panguilemodetalle2014-2015b.csv  |grep 2014 | sed 's/;/ /g' | sed 's/-/ /g' |awk '{print $2 " " $9}'>temperatura_2014.txt
 cat Panguilemodetalle2012-2013.csv Panguilemodetalle2013-2014.csv  |grep 2013 | sed 's/;/ /g' | sed 's/-/ /g' |awk '{print $2  " " $9}'>temperatura_2013.txt
-cat Panguilemodetalle2014-2015a.csv Panguilemodetalle2014-2015b.csv  |grep 2015 | sed 's/;/ /g' | sed 's/-/ /g' |awk '{print $2 " " $9}'>temperatura_2015.txt
+cat Panguilemodetalle2013-2014.csv Panguilemodetalle2014-2015a.csv Panguilemodetalle2014-2015b.csv  |grep 2015 | sed 's/;/ /g' | sed 's/-/ /g' |awk '{print $2 " " $9}'>temperatura_2015.txt
 
 for i in `seq 1 12`;do
 	cat humedad_2010.txt |awk '$1=='$i' {prom+=$2; sum+=1};END {print '$i' ";" prom/sum}'|sed 's/-nan/0/g' >> humedad1_2010.csv
-	cat humedad_2011.txt |awk '$1=='$i' {prom+=$2; sum+=1} ;END {print '$i' ";" prom/sum}'  >> humedad1_2011.csv
-	cat humedad_2012.txt |awk '$1=='$i' {prom+=$2; sum+=1} ;END {print '$i'  ";" prom/sum}' >> humedad1_2012.csv
-	cat humedad_2013.txt |awk '$1=='$i' {prom+=$2; sum+=1} ;END {print '$i'  ";" prom/sum}' >> humedad1_2013.csv
-	cat humedad_2014.txt |awk '$1=='$i' {prom+=$2; sum+=1} ;END {print '$i'  ";" prom/sum}' >> humedad1_2014.csv
+	cat humedad_2011.txt |awk '$1=='$i' {prom+=$2; sum+=1} ;END {print '$i' ";" prom/sum}'  |sed 's/-nan/0/g'>> humedad1_2011.csv
+	cat humedad_2012.txt |awk '$1=='$i' {prom+=$2; sum+=1} ;END {print '$i'  ";" prom/sum}' |sed 's/-nan/0/g' >> humedad1_2012.csv
+	cat humedad_2013.txt |awk '$1=='$i' {prom+=$2; sum+=1} ;END {print '$i'  ";" prom/sum}' |sed 's/-nan/0/g'>> humedad1_2013.csv
+	cat humedad_2014.txt |awk '$1=='$i' {prom+=$2; sum+=1} ;END {print '$i'  ";" prom/sum}'|sed 's/-nan/0/g' >> humedad1_2014.csv
 	cat humedad_2015.txt |awk '$1=='$i' {prom+=$2; sum+=1} ;END {print '$i'  ";" prom/sum}'|sed 's/-nan/0/g' >> humedad1_2015.csv
 
 	cat temperatura_2010.txt |awk '$1 =='$i' {prom+=$2; sum+=1} ;END {print '$i'  ";" prom/sum}'|sed 's/-nan/0/g' >> temperatura1_2010.csv
-	cat temperatura_2011.txt |awk '$1=='$i' {prom+=$2; sum+=1} ;END {print '$i'  ";" prom/sum}' >> temperatura1_2011.csv
-	cat temperatura_2012.txt |awk '$1=='$i' {prom+=$2; sum+=1} ;END {print '$i'  ";" prom/sum}' >> temperatura1_2012.csv
-	cat temperatura_2013.txt |awk '$1=='$i' {prom+=$2; sum+=1} ;END {print '$i'  ";" prom/sum}' >> temperatura1_2013.csv
-	cat temperatura_2014.txt |awk '$1=='$i' {prom+=$2; sum+=1} ;END {print '$i'  ";" prom/sum}' >> temperatura1_2014.csv
+	cat temperatura_2011.txt |awk '$1=='$i' {prom+=$2; sum+=1} ;END {print '$i'  ";" prom/sum}' |sed 's/-nan/0/g'>> temperatura1_2011.csv
+	cat temperatura_2012.txt |awk '$1=='$i' {prom+=$2; sum+=1} ;END {print '$i'  ";" prom/sum}' |sed 's/-nan/0/g'>> temperatura1_2012.csv
+	cat temperatura_2013.txt |awk '$1=='$i' {prom+=$2; sum+=1} ;END {print '$i'  ";" prom/sum}'|sed 's/-nan/0/g' >> temperatura1_2013.csv
+	cat temperatura_2014.txt |awk '$1=='$i' {prom+=$2; sum+=1} ;END {print '$i'  ";" prom/sum}'|sed 's/-nan/0/g' >> temperatura1_2014.csv
 	cat temperatura_2015.txt |awk '$1=='$i' {prom+=$2; sum+=1} ;END {print '$i'";" prom/sum}' |sed 's/-nan/0/g' >> temperatura1_2015.csv
 
 done
@@ -57,11 +66,10 @@ for i in `seq 0  5`; do
 	rm temperatura_201$i.txt
 	mv humedad1_201$i.csv anio_201$i
 	mv temperatura1_201$i.csv anio_201$i
-	#python generateGraphic.py anio_201$i 'img' ./anio_201$i/humedad1_201$i.csv 'meses' 'humedad' 'BK'
-#	python generateGraphic.py anio_201$i 'img1' ./anio_201$i/temperatura1_201$i.csv 'meses' 'humedad' 'BK'
+	python generateGraphic.py anio_201$i 'Humedad' ./anio_201$i/humedad1_201$i.csv 'Meses' 'Humedad' '%Humedad'
+	python generateGraphic.py anio_201$i 'Temperatura' ./anio_201$i/temperatura1_201$i.csv 'Meses' 'Temperatura' '%Temperatura'
 
 done
-	#python generateGraphic.py ./anio_2010 'img' ./anio_2010/humedad1_2010.csv 'meses' 'humedad' 'BK'
-	#python generateGraphic.py anio_2010 'img1' ./anio_2010/temperatura1_2010.csv 'meses' 'humedad' 'BK'
+	
 
  
